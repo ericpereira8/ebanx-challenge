4fed4cc27d530fe3d75529e464dbdc730e59c3eb
Eric <ericpereira8@hotmail.com>
2026-05-10 20:48:47 -0300

    Fix api responses for ipkiss by updating response structure

--- ebanx-challenge/src/main/java/epereira/ebanxchallenge/dto/EventResponseDTO.java
+++ ebanx-challenge/src/main/java/epereira/ebanxchallenge/dto/EventResponseDTO.java
@@ -2,9 +2,17 @@ package epereira.ebanxchallenge.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import lombok.Getter;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
 public class EventResponseDTO {
 
-    public EventResponseDTO(AccountDTO dto, Object o) {
+    private final AccountDTO destination;
+    private final AccountDTO origin;
+
+    public EventResponseDTO(AccountDTO destination, AccountDTO origin) {
+        this.destination = destination;
+        this.origin = origin;
     }
 }
