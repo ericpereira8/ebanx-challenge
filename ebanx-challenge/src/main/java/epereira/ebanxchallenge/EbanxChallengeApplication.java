7432fc1f9ad79fbdae71cc379861879624dbe3d2
Eric Pereira <57161265+ericpereira8@users.noreply.github.com>
2026-05-10 12:46:07 -0300

    Add files via upload
    initial commit

---
+++ ebanx-challenge/src/main/java/epereira/ebanxchallenge/EbanxChallengeApplication.java
@@ -0,0 +1,13 @@
+package epereira.ebanxchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbanxChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbanxChallengeApplication.class, args);
+    }
+
+}
